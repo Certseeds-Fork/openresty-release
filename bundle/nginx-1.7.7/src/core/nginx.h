02043017e39b16b9ad67816fa14a6ccd767e3eb2
Certseeds <51754303+Certseeds@users.noreply.github.com>
2026-03-11 13:06:18 +0000

    feat: publish openresty-1.7.7.2

--- bundle/nginx-1.7.7/src/core/nginx.h
+++ bundle/nginx-1.7.7/src/core/nginx.h
@@ -11,7 +11,7 @@
 
 #define nginx_version      1007007
 #define NGINX_VERSION      "1.7.7"
-#define NGINX_VER          "openresty/" NGINX_VERSION ".1"
+#define NGINX_VER          "openresty/" NGINX_VERSION ".2"
 
 #ifdef NGX_BUILD
 #define NGINX_VER_BUILD    NGINX_VER " (" NGX_BUILD ")"
