d5e9db8ad8fa7f055c757f97d592026eae6a6f5a
Certseeds <51754303+Certseeds@users.noreply.github.com>
2026-03-11 12:54:57 +0000

    feat: publish openresty-1.0.10.48

--- bundle/nginx-1.0.10/src/core/nginx.h
+++ bundle/nginx-1.0.10/src/core/nginx.h
@@ -10,7 +10,7 @@
 
 #define nginx_version      1000010
 #define NGINX_VERSION      "1.0.10"
-#define NGINX_VER          "ngx_openresty/" NGINX_VERSION ".44"
+#define NGINX_VER          "ngx_openresty/" NGINX_VERSION ".48"
 
 #define NGINX_VAR          "NGINX"
 #define NGX_OLDPID_EXT     ".oldbin"
